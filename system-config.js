b3bed8f4c193d04114b7094e83bd6fcb5fb1d6bc
clasb <clas.barthelson@gmail.com>
2016-05-14 11:51:27 +0200

    first page

--- system-config.js
+++ system-config.js
@@ -2,9 +2,48 @@
  * User Configuration.
  **********************************************************************************************/
 /** Map relative paths to URLs. */
-var map = {};
+var map = {
+    'moment': 'vendor/moment/moment.js',
+    '@angular2-material': 'vendor/@angular2-material'
+};
 /** User packages configuration. */
-var packages = {};
+var packages = {
+    '@angular2-material/core': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'core.js'
+    },
+    '@angular2-material/button': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'button.js'
+    },
+    '@angular2-material/input': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'input.js'
+    },
+    '@angular2-material/toolbar': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'toolbar.js'
+    },
+    '@angular2-material/sidenav': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'sidenav.js'
+    },
+    '@angular2-material/list': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'list.js'
+    },
+    '@angular2-material/card': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'card.js'
+    }
+};
 ////////////////////////////////////////////////////////////////////////////////////////////////
 /***********************************************************************************************
  * Everything underneath this line is managed by the CLI.

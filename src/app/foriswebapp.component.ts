87e51c7caec5496fed7aeb8fe25e2d262ea07f8e
clasb <clas.barthelson@gmail.com>
2016-05-14 11:50:34 +0200

    start page

--- src/app/foriswebapp.component.ts
+++ src/app/foriswebapp.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { CORE_DIRECTIVES, FORM_DIRECTIVES } from '@angular/common';
+import { MdButton } from '@angular2-material/button';
+import { MdInput } from '@angular2-material/input';
 
 @Component({
   moduleId: module.id,
   selector: 'foriswebapp-app',
   templateUrl: 'foriswebapp.component.html',
-  styleUrls: ['foriswebapp.component.css']
+  styleUrls: ['foriswebapp.component.css'],
+  directives: [MdButton, MdInput]
 })
 export class ForiswebappAppComponent {
-  title = 'foriswebapp works!';
+	title: 'Foris'
 }
--- src/system-config.ts
+++ src/system-config.ts
@@ -3,10 +3,48 @@
  **********************************************************************************************/
 /** Map relative paths to URLs. */
 const map: any = {
+  'moment': 'vendor/moment/moment.js',
+  '@angular2-material': 'vendor/@angular2-material'
 };
 
 /** User packages configuration. */
 const packages: any = {
+  '@angular2-material/core': {
+    format: 'cjs',
+    defaultExtension: 'js',
+    main: 'core.js'
+  },
+  '@angular2-material/button': {
+    format: 'cjs',
+    defaultExtension: 'js',
+    main: 'button.js'
+  },
+  '@angular2-material/input': {
+    format: 'cjs',
+    defaultExtension: 'js',
+    main: 'input.js'
+  },
+  '@angular2-material/toolbar': {
+    format: 'cjs',
+    defaultExtension: 'js',
+    main: 'toolbar.js'
+  },
+  '@angular2-material/sidenav': {
+    format: 'cjs',
+    defaultExtension: 'js',
+    main: 'sidenav.js'
+  },
+  '@angular2-material/list': {
+    format: 'cjs',
+    defaultExtension: 'js',
+    main: 'list.js'
+  },
+  '@angular2-material/card': {
+    format: 'cjs',
+    defaultExtension: 'js',
+    main: 'card.js'
+  }
+
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
